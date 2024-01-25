b592cfa95f38072304de1150a45c07fdc554fd8d
Prasan <prasan@Prasans-MacBook-Pro.local>
2024-01-25 10:21:41 +0530

    Basic Setup Done and Layout Added

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// For the humburger menu and close icon
+document.getElementById("header__wrapper--mobile").onclick = function () {
+    document.getElementById("header__wrapper--navmobile").classList.toggle("menu_toggle");
+};
